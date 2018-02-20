4f44143dbf35b0a947acd96bf75e1853292d9a74
Mickey English <mickeyenglish@Mickeys-MacBook-Pro.local>
2018-02-20 00:09:49 +0000

    users can join and leave groups

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@
 
   
   resources :chatrooms do 
-  	resources :chatroom_users 
+  	resource :chatroom_users 
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
