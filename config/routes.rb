c8de73f31242d5e7fc5fbc6f6334c2dc7ee9a642
Mickey English <mickeyenglish@Mickeys-MacBook-Pro.local>
2018-02-22 00:11:16 +0000

    Chat working wothout action cable

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@
   
   resources :chatrooms do 
   	resource :chatroom_users 
+  	resource :messages 
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
