c9b31ef6286b5e290f88bcbdcc9587e9c482e663
Mickey English <mickeyenglish@Mickeys-MacBook-Pro.local>
2018-03-13 20:20:39 +0000

    CRUD working for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,28 @@
 class PostsController < ApplicationController
 
 	def index
-		@posts = Post.all
+		@posts = Post.all.order('created_at DESC')
 	end 
 
 	def show
+		@post = Post.find(params[:id])
 	end
 
 	def new
+		@post = Post.new
 	end
 
 	def create
+		@post = Post.new(post_params)
+
+
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
+
+
 	end
 
 	def update
@@ -18,5 +30,11 @@ class PostsController < ApplicationController
 
 	def edit
 	end 
+
+	private 
+
+	def post_params
+		params.require(:post).permit(:title, :content)
+	end
 
 end
