cf23a58f57a672c53c2a9f9ffb1cd566d1d4909c
Mickey English <mickeyenglish@Mickeys-MacBook-Pro.local>
2018-03-14 13:06:22 +0000

    added edit, back and delte buttons to form for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,9 +26,17 @@ class PostsController < ApplicationController
 	end
 
 	def update
+		@post = Post.find(params[:id])
+
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end
 	end 
 
 	def edit
+		@post = Post.find(params[:id])
 	end 
 
 	private 
